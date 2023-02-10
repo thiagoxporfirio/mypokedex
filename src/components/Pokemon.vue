491e93fcc5a90998818c1e5badeb2c1f83717141
Thiago Porfirio <thiagovporfirio@gmail.com>
2023-02-10 14:53:23 -0300

    finalizado o script do componente pokemon search

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,68 +1,72 @@
 <template>
-    <div class="container">
-      <h1>POKEDEX</h1>
-      <PokemonList :imageUrl="imageUrl" :apiUrl="apiUrl" @setPokemonUrl="setPokemonUrl" />
-      <PokemonDetail v-if="showDetail" :pokemonUrl="pokemonUrl" :imageUrl="imageUrl" @closeDetail="closeDetail" />
-    </div>
+  <div class="container">
+    <h1>POKEDEX</h1>
+    <PokemonSearch :apiUrl="apiUrl" @setPokemonUrl="setPokemonUrl" />
+    <PokemonList :imageUrl="imageUrl" :apiUrl="apiUrl" @setPokemonUrl="setPokemonUrl" />
+    <PokemonDetail v-if="showDetail" :pokemonUrl="pokemonUrl" :imageUrl="imageUrl" @closeDetail="closeDetail" />
+  </div>
 </template>
   
-  <script>
-    import PokemonDetail from './PokemonDetail.vue';
-    import PokemonList from './PokemonList.vue';
- 
-    export default {
+<script>
+import PokemonDetail from './PokemonDetail.vue';
+import PokemonList from './PokemonList.vue';
 
-      data: () => {
-        return {
-          imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/',
-          apiUrl: 'https://pokeapi.co/api/v2/pokemon/',
-          pokemonUrl: '',
-          showDetail: false
-        }
-      },
-      components: {
+export default {
+
+  data: () => {
+    return {
+      imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/',
+      apiUrl: 'https://pokeapi.co/api/v2/pokemon/',
+      pokemonUrl: '',
+      showDetail: false
+    }
+  },
+  components: {
     PokemonList,
-    PokemonDetail
-},
-      methods: {
-        setPokemonUrl(url) {
-          this.pokemonUrl = url
-          this.showDetail = true
-        },
-        closeDetail() {
-          this.pokemonUrl = ''
-          this.showDetail = false
-        }
-      }
-    
+    PokemonDetail,
+    PokemonSearch
+  },
+  methods: {
+    setPokemonUrl(url) {
+      this.pokemonUrl = url
+      this.showDetail = true
+    },
+    closeDetail() {
+      this.pokemonUrl = ''
+      this.showDetail = false
     }
-  </script>
-  
-  <style lang="scss" scoped>
- @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@500&display=swap');
-  .container {
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    padding: 10px;
-    width: calc(100% - 20px);
-    min-height: calc(100vh - 20px);
-    background: radial-gradient(#0074ff, #20dac0);
-    font-family: 'Nunito', sans-serif;
-    font-size: 1rem;
-    font-weight: normal;
-
   }
-  h1 {
-      color: #fff;
-      background: #f1efed;
-      box-shadow: 0 15px 30px rgba(0,0,0,.2),
-                    0 10px 10px rgba(0,0,0,.2);
-      padding: 20px;
-      border-radius: 18px;
-      color: #120907;
-      border: 1px solid rgba(0,0,0,0.8);
-    }
+
+}
+</script>
+  
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@500&display=swap');
+
+.container {
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 10px;
+  width: calc(100% - 20px);
+  min-height: calc(100vh - 20px);
+  background: radial-gradient(#0074ff, #20dac0);
+  font-family: 'Nunito', sans-serif;
+  font-size: 1rem;
+  font-weight: normal;
+
+}
+
+h1 {
+  color: #fff;
+  background: #f1efed;
+  box-shadow: 0 15px 30px rgba(0, 0, 0, .2),
+    0 10px 10px rgba(0, 0, 0, .2);
+  padding: 20px;
+  border-radius: 18px;
+  color: #120907;
+  border: 1px solid rgba(0, 0, 0, 0.8);
+}
 </style>
---
+++ src/components/PokemonSearch.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="searchbar">
+        <form @submit.prevent="setPokemonUrl">
+            <input type="text" v-model="searchvalue">
+        </form>
+        <i class="fas fa-search" @click="setPokemonUrl"></i>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'apiUrl'
+    ],
+    data: () => {
+
+        return {
+            searchvalue: '',
+
+        }
+    },
+    methods: {
+
+        setPokemonUrl(url) {
+            if (this.searchvalue !== '')
+                this.$emit('setPokemonUrl', this.apiUrl + this.searchvalue);
+        }
+
+    }
+}
+</script>
+
+<style>
+
+</style>
